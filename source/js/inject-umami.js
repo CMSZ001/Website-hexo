4232d1d5212a7e0da9fc07619602d8bac9aca2f9
CMSZ <git@acmsz.top>
2025-01-24 21:58:41 +0800

    fix: 修改Umami脚本注入位置

--- source/js/inject-umami.js
+++ source/js/inject-umami.js
@@ -1,7 +1,5 @@
-hexo.extend.injector.register("body_end", () => {
-  // 替换为你的 Umami 脚本地址和网站 ID
-  const umamiScript = `
-    <script defer src="https://umami.acmsz.top/c4573967-e300-e2d0-6f2e-368c9dd37be0" data-website-id="4164eba8-0bbc-45ad-aa1c-ce2e2d5a4cc1"></script>
-  `;
-  return umamiScript;
-}, "default");
+hexo.extend.injector.register(
+"head_begin",
+'<script defer src="https://umami.acmsz.top/c4573967-e300-e2d0-6f2e-368c9dd37be0" data-website-id="4164eba8-0bbc-45ad-aa1c-ce2e2d5a4cc1"></script>',
+"default",
+);
